df20808b863ada579aaf24bc61ea5cdbb18ca54b
wladweb <wladwebwork@gmail.com>
2020-05-03 21:22:53 +0300

    ads

---
+++ Assets/Scripts/Ads/AdsForGold.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+using UnityEngine.UI;
+
+public class AdsForGold : MonoBehaviour, IUnityAdsListener
+{
+    [SerializeField] private GameObject _startWatchingButton;
+
+    private string _gameId = "3585785";
+    private string _myPlacementId = "rewardedVideo";
+    private bool _testMode = true;
+
+    private void Start()
+    {
+        Advertisement.AddListener(this);
+        Advertisement.Initialize(_gameId, _testMode);
+
+        _startWatchingButton.GetComponent<Button>().onClick.AddListener(() =>
+        {
+            Advertisement.Show(_myPlacementId);
+            _startWatchingButton.SetActive(false);
+        });
+    }
+
+    public void OnUnityAdsDidError(string message)
+    {}
+
+    public void OnUnityAdsDidFinish(string placementId, ShowResult showResult)
+    {
+        if (showResult == ShowResult.Finished)
+        {
+            Debug.Log("Reward");
+        }
+        else if (showResult == ShowResult.Failed)
+        {
+            //
+        }
+        else if (showResult == ShowResult.Skipped)
+        { 
+            //
+        }
+    }
+
+    public void OnUnityAdsDidStart(string placementId)
+    {}
+
+    public void OnUnityAdsReady(string placementId)
+    {
+        if (_myPlacementId == placementId)
+        {
+            _startWatchingButton.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/Ads/AdsInit.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Advertisements;
+
+public class AdsInit : MonoBehaviour
+{
+    private string gameId = "3585785";
+    private bool testMode = true;
+
+    private void Start()
+    {
+        Advertisement.Initialize(gameId, testMode);
+        StartCoroutine(ShowBannerWhenReady());
+    }
+
+    private IEnumerator ShowBannerWhenReady()
+    {
+        while (!Advertisement.IsReady("MainBottom"))
+        {
+            yield return new WaitForSeconds(.5f);
+        }
+
+        Advertisement.Banner.SetPosition(BannerPosition.BOTTOM_CENTER);
+        Advertisement.Banner.Show("MainBottom");
+    }
+}
