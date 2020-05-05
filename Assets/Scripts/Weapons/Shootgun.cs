2c2cc50c299192958c423e8cc02483a44c921c84
wladweb <wladwebwork@gmail.com>
2020-05-05 09:14:19 +0300

    add shootgun

---
+++ Assets/Scripts/Weapons/Shootgun.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shootgun : Weapon
+{
+    public override void Shoot(Transform shootPoint)
+    {
+        Instantiate(Bullet, shootPoint.transform.position, Quaternion.identity);
+    }
+}
